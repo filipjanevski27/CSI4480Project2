afad0e48d6af29de79c55923ff53dbc11ca089e4
Ryan Raymond <rjraymond@oakland.edu>
2022-12-07 09:34:59 -0800

    Moved imports in preperation for module check.

--- ransomware.py
+++ ransomware.py
@@ -1,7 +1,10 @@
-from cryptography.fernet import Fernet, InvalidToken
 import base64
 import os
 import sys
+
+# ToDo: Check for cryptography library
+
+from cryptography.fernet import Fernet, InvalidToken
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
